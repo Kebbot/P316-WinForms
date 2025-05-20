b5116dabf4b6cd810534451e45c39fc1697e50f3
Nikolz <126600682+Kebbot@users.noreply.github.com>
2025-05-20 21:02:04 +0300

    Commit#1
    Commit#1

---
+++ P316Win/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace P316Win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Utime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aquamarine;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(366, 188);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "НАЖМИ МЕНЯ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество секунд работы таймера";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(636, 23);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(456, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Стартуем!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(623, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "СТОП!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Utime
+            // 
+            this.Utime.AutoSize = true;
+            this.Utime.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Utime.Location = new System.Drawing.Point(514, 356);
+            this.Utime.Name = "Utime";
+            this.Utime.Size = new System.Drawing.Size(0, 39);
+            this.Utime.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Utime);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Моя программа";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label Utime;
+    }
+}
+
---
+++ P316Win/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P316Win
+{
+    public partial class Form1 : Form
+    {
+       Timer vTimer = new Timer();
+        private static Timer Utimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            button3.Enabled = false;
+            //определяем обработчик события для таймера
+            vTimer.Tick += new EventHandler(ShowTimer);
+            //button1.Hide();
+
+            /////////////////////
+            Utime.Text = DateTime.Now.ToLongTimeString();
+            Utimer.Tick += new EventHandler(ShowTime);
+            Utimer.Interval = 500;
+            Utimer.Start();
+        }
+        private void ShowTime(object vObject, EventArgs e)
+        {
+            Utime.Text = DateTime.Now.ToLongTimeString();
+        }
+        private void ShowTimer(object vObject, EventArgs e)
+        {
+            vTimer.Stop();
+            button3.Enabled = false;
+            button2.Enabled = true;
+            MessageBox.Show("Таймер отработал!", "Таймер");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String message = "Окно для проверки тествого сообщения";
+            MessageBox.Show(message);
+
+        }
+        public delegate void MouseEventHendler(object sender, MouseEventArgs e);
+        private String CoordinatesToString(MouseEventArgs e)
+        {
+            return "Координаты мыши: X = " + e.X.ToString() + "; Y = " + e.Y.ToString();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = CoordinatesToString(e);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            String message = "";
+            if(e.Button == MouseButtons.Right)
+            {
+                message = "Вы нажали правую кнопку мыши. ";
+            }
+            if(e.Button == MouseButtons.Left)
+            {
+                message = "Вы нажали левую кнопку мыши. ";
+            }
+            message += "\n" + CoordinatesToString(e);
+
+            String caption = "Клик мыши";
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(numericUpDown1.Value < 5)
+            {
+                MessageBox.Show("Количество секунд должно быть не меньше 5!");
+                return;
+            }
+            button3.Enabled = true;
+
+            vTimer.Interval = Decimal.ToInt32(numericUpDown1.Value) * 1000;
+            button2.Enabled = false;
+            vTimer.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vTimer.Stop();
+            MessageBox.Show("Таймер не успел отработать!", "Таймер");
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+    }
+
+}
